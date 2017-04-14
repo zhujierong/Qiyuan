751a345f59f1e9037bac498b45dd1afa75631d1e
zhujierong <505744817@qq.com>
2017-04-14 16:59:01 +0800

    2017年4月14日

--- Qiyuan/js/JRZH.js
+++ Qiyuan/js/JRZH.js
@@ -1,5 +1,17 @@
 var JRZH = {
 	styles: {
-		pageContHeight: '0.44rem',
+		HeaderHeight: '44px',
+	},
+	openWindow: function() {},
+	navgationInit: function() {
+
+	},
+	filterInit: function() {
+		var ul_width = 0;
+		var filter_tab_count = mui(".filter-tab ul li").length;
+		mui(".filter-tab ul li").each(function() {
+			//			ul_width += this.outerWidth()
+		});
+		console.log(ul_width)
 	}
 }
--- Qiyuan/js/common.js
+++ Qiyuan/js/common.js
@@ -8,3 +8,44 @@
 		};
 	a.addEventListener && (d.addEventListener(e, c, !1), a.addEventListener("DOMContentLoaded", c, !1))
 })(document, window);
+//nav
+JRZH.openWindow = function(url) {
+	mui.openWindow({
+		url: url,
+		id: url,
+		styles: {
+			top: JRZH.styles.HeaderHeight, //新页面顶部位置
+			bottom: "0", //新页面底部位置
+		},
+		extras: {
+			//自定义扩展参数，可以用来处理页面间传值
+		},
+		createNew: false, //是否重复创建同样id的webview，默认为false:不重复创建，直接显示
+		show: {
+			autoShow: true, //页面loaded事件发生后自动显示，默认为true
+			event: 'titleUpdate', //页面显示时机，默认为titleUpdate事件时显示
+			aniShow: "pop-in",
+			extras: {} //窗口动画是否使用图片加速
+		},
+		waiting: {
+			autoShow: false, //自动显示等待框，默认为true
+			title: '正在加载...', //等待对话框上显示的提示内容
+			options: {}
+		}
+	})
+}
+mui(".mui-bar-tab").on("tap", ".home", function() {
+	console.log("home")
+	JRZH.openWindow("../home/index.html");
+});
+mui(".mui-bar-tab").on("tap", ".server", function() {
+	JRZH.openWindow("../server/list_main.html");
+});
+mui(".mui-bar-tab").on("tap", ".community", function() {
+	JRZH.openWindow("../home/index.html");
+});
+mui(".mui-bar-tab").on("tap", ".mine", function() {
+	JRZH.openWindow("../home/index.html");
+});
+//筛选宽度计算
+JRZH.filterInit();
