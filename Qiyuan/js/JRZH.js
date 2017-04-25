eb3f761e690f66b1ba88ee1d666d9a18bd391e8e
zhujierong <505744817@qq.com>
2017-04-25 18:05:08 +0800

    2017年4月25日

--- Qiyuan/js/JRZH.js
+++ Qiyuan/js/JRZH.js
@@ -1,17 +1,39 @@
 var JRZH = {
 	styles: {
-		HeaderHeight: '44px',
+		headerHeight: '44px',
+		menuHeight: '50px',
 	},
 	openWindow: function() {},
 	navgationInit: function() {
 
 	},
 	filterInit: function() {
+		var _div_wrap = mui("#sliderSegmentedControl");
 		var ul_width = 0;
-		var filter_tab_count = mui(".filter-tab ul li").length;
-		mui(".filter-tab ul li").each(function() {
-			//			ul_width += this.outerWidth()
+		var filter_tab_count = mui(".mui-segmented-control.mui-segmented-control-inverted .mui-control-item").length;
+		var window_width = window.screen.width;
+		var width_count = 0;
+		mui(".mui-segmented-control.mui-segmented-control-inverted .mui-control-item").each(function() {
+			width_count += this.offsetWidth;
 		});
-		console.log(ul_width)
+		if(width_count < window_width) {
+			var ele1 = document.querySelectorAll("#sliderSegmentedControl .mui-scroll")[0];
+			if(!ele1) return;
+			ele1.style.width = "100%";
+			var elem = document.querySelectorAll(".mui-segmented-control.mui-segmented-control-inverted .mui-control-item")
+			mui(elem).each(function() {
+				this.style.width = 100 / filter_tab_count + "%";
+				this.style.padding = 0;
+				this.style.float = "left";
+			});
+		}
+	},
+	reg: {
+		mobile: function(text) {
+			return /^1[34578]\d{9}$/.test(text);
+		},
+		email: function(text) {
+			return /^[a-zA-Z0-9][a-zA-Z0-9_\\.]+[a-zA-Z0-9]@[a-z0-9]{2,7}(\\.[a-z]{2,3}){1,3}$/.test(text);
+		}
 	}
 }
